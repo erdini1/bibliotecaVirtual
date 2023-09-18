2b27766cb8c8961a8096112f440f11d6b29c4e51
Dylan Erdini <erdini.dylan@gmail.com>
2023-09-18 14:20:53 -0300

    Created the constants folder

---
+++ constants/writers.js
@@ -0,0 +1,44 @@
+const writers = [
+    {
+        id: 1,
+        name: "Jorge Luis",
+        username: "Borges",
+        birthYear: "24/08/1899",
+        books: [
+            {
+                id: 1,
+                title: "Ficciones",
+                description: "Se trata de uno de sus mas...",
+                publicationYear: 1944
+            },
+            {
+                id: 2,
+                title: "El Aleph",
+                description: "Otra recopilación de cuentos...",
+                publicationYear: 1949
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: "Brandon",
+        username: "Sanderson",
+        birthYear: "19/12/1975",
+        books: [
+            {
+                id: 1,
+                title: "El camino de los reyes",
+                description: "Es una novela de fantasía epica...",
+                publicationYear: 2010
+            },
+            {
+                id: 2,
+                title: "El imperio final",
+                description: "El imperio final inicia la saga Nacidos...",
+                publicationYear: 2006
+            }
+        ]
+    },
+]
+
+export default writers
