70154ac1c68939e8e3facc85c614c87b6cfebe52
Dylan Erdini <erdini.dylan@gmail.com>
2023-09-19 13:01:17 -0300

    Added the addWriter controller

--- src/controllers/authorsController.js
+++ src/controllers/authorsController.js
@@ -5,6 +5,14 @@ exports.allWriters = (req, res) => {
 }
 
 exports.addWriter = (req, res) => {
+    const { name, username, birthYear } = req.body
+    writers.push({
+        id: writers.length == 0 ? 0 : writers.length,
+        name,
+        username,
+        birthYear,
+        books: []
+    })
     return res.status(201).json({ msg: "Writer added" })
 }
 
@@ -12,7 +20,7 @@ exports.oneWriter = (req, res) => {
     const id = +req.params.id
     const author = writers.find(element => element.id == id)
     if (!author) {
-        return res.status(404).json({ error: "Author doesn't exists" })
+        return res.status(404).json({ error: "Author doesn't exist" })
     }
     return res.status(200).json(author)
 }
